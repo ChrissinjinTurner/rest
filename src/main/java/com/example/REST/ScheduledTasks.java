9a9317d49c171b9537e4ead2fc422a67f7713cde
Chris Turner <Chrissinjinturner@gmail.com>
2017-11-01 13:47:07 -0700

    Fixed scheduled task

--- src/main/java/com/example/REST/ScheduledTasks.java
+++ src/main/java/com/example/REST/ScheduledTasks.java
@@ -4,30 +4,32 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Date;
-
 @Component
 public class ScheduledTasks {
 
     RestTemplate restTemplate = new RestTemplate();
 
-//    @Scheduled (fixedRate = 5000)
-//    public void periodidcTask1() {
-//        System.out.println("The time now is: " + new Date());
-//    }
+    private void doUpdate(String newMessage) {
+        String putUrl = "http://localhost:8080/updateGreeting";
+        restTemplate.put(putUrl, newMessage);
+    }
 
-    @Scheduled(cron = "*/5 * * * * *")
-    public void getGreeting() {
-        String url = "http://localhost:8080/greeting";
-        Greeting greeting = restTemplate.getForObject(url, Greeting.class);
-        System.out.println(greeting.getContent());
+    private Greeting getGreeting() {
+        String getUrl = "http://localhost:8080/greeting";
+        return restTemplate.getForObject(getUrl, Greeting.class);
     }
 
     @Scheduled(cron = "*/5 * * * * *")
-    public void postGreeting() {
-        String url = "http://localhost:8080/createGreeting";
-        Greeting greeting = restTemplate.postForObject(url, "Bye World", Greeting.class);
-        System.out.println(greeting.getContent());
-        Greeting greeting1 = restTemplate.postForObject(url, "Hello World", Greeting.class);
+    public void task1() {
+        doUpdate("Hello World");
+        System.out.println(getGreeting().getContent());
     }
+
+    @Scheduled(cron = "*/4 * * * * *")
+    public void task2() {
+        doUpdate("Bye World");
+        System.out.println(getGreeting().getContent());
+    }
+
+
 }
